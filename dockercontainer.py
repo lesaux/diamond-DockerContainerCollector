1dd789e3551f6873241c53f342b4758c97e8dbfb
Pierig Le Saux <lesaux@pythian.com>
2016-04-23 23:06:36 -0400

    update for docker v1.11 - stats array has changed

--- dockercontainer.py
+++ dockercontainer.py
@@ -47,37 +47,43 @@ class DockerContainerCollector(diamond.collector.Collector):
     def collect(self):
         def print_metric(cc,name):
             data = cc.stats(name)
+            #self.log.debug(data)
             metrics = json.loads(data.next())
+            #self.log.debug(metrics)
             if name.find("/") != -1:
                 name = name.rsplit('/',1)[1]
             #memory metrics
             self.memory = self.flatten_dict (metrics['memory_stats'])
+            #self.log.debug(self.memory)
             for key, value in self.memory.items():
-                metric_name = name+".memory."+key
-                self.publish_gauge(metric_name, value)
+                if type(value) == int:
+                    metric_name = name+".memory_stats."+key
+                    #self.log.debug(metric_name)
+                    #self.log.debug(value)
+                    self.publish_gauge(metric_name, value)
             #cpu metrics
             self.cpu = self.flatten_dict (metrics['cpu_stats'])
             for key, value in self.cpu.items():
                 #percpu_usage is a list, we'll deal with it after
                 if type(value) == int:
-                    metric_name = name+".cpu."+key
+                    metric_name = name+".cpu_stats."+key
                     self.publish_counter(metric_name, value)
                 #dealing with percpu_usage
                 if type(value) == list:
                     self.length = len(value)
                     for i in range(self.length):
                         self.value = value
-                        self.metric_name = name+".cpu."+key+str(i)
+                        self.metric_name = name+".cpu_stats."+key+str(i)
                         self.publish_counter(self.metric_name, self.value[i])
             #network metrics
             self.network = self.flatten_dict (metrics['networks'])
             for key, value in self.network.items():
-                metric_name = name+".network."+key
+                metric_name = name+".networks."+key
                 self.publish_counter(metric_name, value)
             #blkio metrics
             self.blkio = self.flatten_dict (metrics['blkio_stats'])
             for key, value in self.blkio.items():
-                metric_name = name+".blkio."+key
+                metric_name = name+".blkio_stats."+key
                 self.publish_counter(metric_name, value)
 
         cc = docker.Client(base_url='unix://var/run/docker.sock', version='auto')
