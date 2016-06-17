44b2c36d2cb907daee05d1b5669b95a3de642baa
Pierig Le Saux <lesaux@pythian.com>
2016-06-17 10:07:56 -0400

    removing -l parameter (log to stdout)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 /config_diamond.sh
-/usr/local/bin/diamond -f --skip-pidfile -l -c /etc/diamond/diamond.conf
+#/usr/local/bin/diamond -f --skip-pidfile -l -c /etc/diamond/diamond.conf
+/usr/local/bin/diamond -f --skip-pidfile -c /etc/diamond/diamond.conf
